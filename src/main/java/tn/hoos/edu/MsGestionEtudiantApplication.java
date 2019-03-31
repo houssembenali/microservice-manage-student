ed615f258d4e646414b0c7d95bfecdd40ee46949
Houssem Ben Ali <houssembenali90@gmail.com>
2019-03-31 13:02:59 +0100

    Added my project

---
+++ src/main/java/tn/hoos/edu/MsGestionEtudiantApplication.java
@@ -0,0 +1,13 @@
+package tn.hoos.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionEtudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionEtudiantApplication.class, args);
+	}
+
+}
